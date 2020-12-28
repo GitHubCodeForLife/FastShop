0b8308a5040d43e94871c5f64357008936514fbe
FoveIT <65528999+GitHubCodeForLife@users.noreply.github.com>
2020-12-28 14:12:34 +0700

    Search, comment

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,4 +1,4 @@
-const productServices = require('../model/productsServices');
+const productsServices = require('../model/productsServices');
 const { ObjectId} = require('mongodb');
 
 exports.index = async (req, res, next)=>{
@@ -7,7 +7,7 @@ exports.index = async (req, res, next)=>{
       if(!req.query.id)
         res.render('./product/products',{title: 'Product Page',user: req.user,isProducts: isProducts});
        else {
-            let product = await productServices.findOne({_id:new ObjectId(req.query.id)});
+            let product = await productsServices.findOne({_id:new ObjectId(req.query.id)});
             res.render('./product/detail',{title: `${product.title} Detail Page`,user: req.user, product: product,isProducts: isProducts});
        }
 }
---
+++ model/commentServices.js
@@ -0,0 +1,8 @@
+const {db} = require('../database/db');
+const { ObjectId} = require('mongodb');
+
+exports.find = async(key,page,  perPage)=>{
+    console.log('Find commnents');
+    const ans =  await db().collection('comments').find(key).limit(perPage).skip((page-1)*perPage).toArray();
+    return ans;
+}
